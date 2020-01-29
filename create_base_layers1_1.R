294ab4032f6b7605bc71d38cd085542eeca09b91
blabohm <benjamin.labohm@gmail.com>
2020-01-29 12:18:57 +0100

    Creating base layers and the functions required in the skript
    Both documents are needed for running successfully. Also, folder paths might need adjustment. The skript takes the following input: City name, either urban atlas file for city or rasterized urban atlas file, location of city center (point-shape), distance to transport classes raster and distance to dense residential classes raster.

---
+++ create_base_layers1_1.R
@@ -0,0 +1,280 @@
+################################################################################
+#
+# This document contains the workflow for creating the base layers required for 
+# the implementation of land-use-change scenarios in QuickScan using urban-
+# atlas data of European cities.
+# 
+# Created by: Benjamin Labohm, Berlin 2019
+#
+################################################################################
+
+# load required packages
+library(rgdal)
+library(raster)
+library(dplyr)
+
+################################################################################
+# Load input files
+################################################################################
+
+# city name
+city <- "Stockholm"
+# data direction:
+data_dir <- "C:/LandOeko/ENABLE/"
+# input direction
+input_dir <- paste0(data_dir,
+                    city,
+                    "/input/")
+# output direction
+output_dir <- paste0(data_dir, 
+                     city,
+                     "/output/")
+if (!dir.exists(output_dir)) dir.create(output_dir)
+# load functions (make sure this loads newest version, in case you make changes)
+data_dir %>% 
+  list.files(recursive = T,
+             full.names = T) %>% 
+  grep("functions",. ,  value = T) %>% 
+  .[1] %>% 
+  source()
+
+# get files
+
+## land-use raster (IF urban atlas data is already rasterized)
+rstfile <- 
+  paste0(data_dir,
+         city) %>% 
+  list.files(recursive = T,
+             full.names = T) %>% 
+  grep("LU_2012.tif",. ,  value = T) %>% 
+  .[1]
+
+## ELSE load land use shape
+lu <- 
+  if (file.exists(rstfile)) rstfile %>% 
+  raster() else input_dir %>% 
+  list.files(recursive = T,
+             full.names = T,
+             pattern = ".shp$") %>% 
+  grep("LU[[:digit:]]",. ,  value = T) %>% 
+  readOGR(stringsAsFactors = F) %>% 
+  rasterize.ua(output_dir, .)
+
+
+# causes "Error in .local(.Object, ...) :"
+## reload raster file because ...
+#lu <- 
+#  rstfile %>% 
+#  raster()
+
+## write land-use raster to output folder to create a .prj file
+#writeRaster(lu,
+#            paste0(output_dir,
+#                   "LU_2012.tif"),
+#            datatype = "INT2U",
+#            format = "GTiff",
+#            overwrite = T,
+#            prj = T,
+#            options = c("TFW=YES"))
+
+## center location
+center <- 
+  input_dir %>% 
+  list.files(recursive = T,
+             full.names = T,
+             pattern = ".shp") %>% 
+  grep("center",. ,  value = T) %>% 
+  readOGR()
+projection(center) <- projection(lu)
+
+## distance to transport raster (previously created with proximity raster 
+## function in qgis)
+d2_transport <-
+  input_dir %>% 
+  list.files(recursive = T,
+             full.names = T,
+             pattern = ".tif") %>% 
+  grep("trans",. ,  value = T) %>% 
+  raster()
+projection(d2_transport) <- projection(lu)
+
+## distance to residential raster (previously created with proximity raster 
+## function in qgis)
+d2_densres <- 
+  input_dir %>% 
+  list.files(recursive = T,
+             full.names = T,
+             pattern = ".tif") %>% 
+  grep("densres",. ,  value = T) %>% 
+  raster()
+projection(d2_densres) <- projection(lu)
+
+
+################################################################################
+# Step 1: Probability of Densification
+# Create distance raster and calculate tertiles (for 'close', 'mid', 'far')
+################################################################################
+
+## calculate distance raster
+d2center <- distanceFromPoints(lu, center@coords[1, c(1:2)])
+## mask distance raster to get only values for discontinuous urban fabric classes
+### select classes
+duf_classes <- c(11210, 11220, 11230, 11240)
+### create mask
+duf_mask <- create.mask(lu, duf_classes)
+### mask distance raster
+d2center_masked <- mask(d2center, 
+                        duf_mask) 
+## calulate tertiles
+d2center_ter <- d2center_masked %>% calc.ter()
+## write distance raster in tertiles to file
+writeRaster(d2center_ter,
+            paste0(output_dir, "d_res2center.tif"),
+            datatype = "INT2S",
+            format = "GTiff",
+            overwrite = T)
+
+################################################################################
+# Step 1: Probability of Densification
+# Group urban fabric classes in density classes:
+# 1: Class 11100: very dense,  2: Classes 11210-11220: dense, 
+# 3: Classes 11230-11240: low dense
+################################################################################
+
+# create reclassification table
+rcl_d <- matrix(
+  data = c(
+    -Inf, 11098, 0,
+    11099, 11101, 1,
+    11209, 11221, 2,
+    11229, 11241, 3,
+    11242, Inf, 0),
+  nrow = 5,
+  ncol = 3,
+  byrow = T)
+# reclassify land-use raster
+dens_rst <- reclassify(x = lu,
+                       rcl = rcl_d)
+# write density raster to file
+writeRaster(dens_rst,
+            paste0(output_dir, "dens_resid.tif"),
+            datatype = "INT2S",
+            format = "GTiff",
+            overwrite = T)
+
+################################################################################
+# Step 1: Probability of Densification
+# Create distance to transport raster and calculate tertiles 
+# (for 'close', 'mid', 'far') for:
+#       - discontinuous residential (11240, 11230, 11220 and 11210)
+#       - arable land, pastures and forest (21000, 23000, 31000) 
+################################################################################
+
+# create transportation raster (classes 12210 and 12220) 
+# for creation of distance to transport raster in qgis!
+#transport_classes <- c(12210, 12220)
+#transport_rst <- create.mask(lu, transport_classes)
+##calculate distance to transport line (takes ~1.5h; not parallizable faster 
+# in QGIS. But beware: QGIS takes map units(Pixels) not meters. Influence on 
+# tertile calculation?)
+#d2_transport <- distance(transport_rst)
+#writeRaster(d2_transport,
+#            paste0(data_dir, "distance_trans.tif"),
+#            datatype = "INT2S",
+#            format = "GTiff",
+#            overwrite = T)
+
+# mask distance raster to get only values for low and very low density
+# discontinuous urban fabric classes 11240, 11230, 11220 and 11210
+ld_res_classes <- c(11240, 11230, 11220, 11210)
+ld_res_mask <- create.mask(lu, ld_res_classes) 
+## mask distance raster 
+dld_res2trans <- mask(d2_transport, 
+                      ld_res_mask)
+
+## calulate tertiles
+dld_res2trans_ter <- calc.ter(dld_res2trans)
+
+## write distance rasters to file
+writeRaster(dld_res2trans_ter,
+            paste0(output_dir, "d_lowdens2trans.tif"),
+            datatype = "INT2S",
+            format = "GTiff",
+            overwrite = T)
+
+################################################################################
+# Step 2: Probability of new construction
+################################################################################
+# mask distance raster to get only values for arable land, pastures and forest 
+apf_classes <- c(21000, 23000, 31000, 32000)
+apf_mask <- create.mask(lu, apf_classes)
+## mask distance raster 
+dapf2trans <- mask(d2_transport, 
+                   apf_mask)
+## calulate tertiles
+dapf2trans_ter <- calc.ter(dapf2trans)
+
+writeRaster(dapf2trans_ter,
+            paste0(output_dir, "d_green2trans.tif"),
+            datatype = "INT2S",
+            format = "GTiff",
+            overwrite = T)
+
+################################################################################
+# Create distance of arable land, pastures and forest (21000, 23000, 31000) 
+# to medium and dense urban farbric raster (class 11100, 11210, 11220) 
+################################################################################
+
+# Create distance raster
+## use arable, patures, forest classes from previous section to mask distance to
+## dense residential
+dapf2res <- mask(d2_densres, 
+                 apf_mask)
+## calulate tertiles
+dapf2res_ter <- calc.ter(dapf2res)
+## write results to file
+writeRaster(dapf2res_ter,
+            paste0(output_dir, "d_green2res.tif"),
+            datatype = "INT2S",
+            format = "GTiff",
+            overwrite = T)
+
+################################################################################
+# Step 3: Conversion within built-up
+################################################################################
+
+built_classes <- c(12100, 11300, 12220, 12230, 13100, 13300, 13400)
+built_mask <- create.mask(lu, built_classes) 
+## mask distance to residential raster 
+d_built2res <- mask(d2_densres, 
+                    built_mask)
+
+### mask distance to center raster
+d_built2center <- mask(d2center, 
+                       built_mask) 
+
+## calulate tertiles
+d_built2res_ter <- calc.ter(d_built2res)
+
+d_built2center_ter <- calc.ter(d_built2center)
+
+## write distance rasters to file
+writeRaster(d_built2res_ter,
+            paste0(output_dir, "d_built2res.tif"),
+            datatype = "INT2S",
+            format = "GTiff",
+            overwrite = T)
+
+writeRaster(d_built2center_ter,
+            paste0(output_dir, "d_built2center.tif"),
+            datatype = "INT2S",
+            format = "GTiff",
+            overwrite = T)
+
+################################################################################
+# Create no change layers
+################################################################################
+# load lu change and protection files
+
+if (paste0(output_dir, "no_change.tif") %>% 
+    file.exists() == F) rasterize.nc(input_dir, output_dir)
---
+++ functions.R
@@ -0,0 +1,170 @@
+################################################################################
+# Functions required to create base layers for QuickScan
+################################################################################
+
+
+################################################################################
+# Calculate tertiles of any given raster
+# input_rst = a raster with continuous numeric values 
+################################################################################
+
+calc.ter <- function(input_rst){
+  # load packages
+  require(raster)
+  require(dplyr)
+  ## calulate tertiles 
+  tertile <- quantile(input_rst, probs = c(1 / 3, 2 / 3))
+  
+  ## reclassify matrix for tertiles
+  rcl_t <- matrix(
+    data = c(
+      -Inf, tertile[1], 1,
+      tertile[1] + .00001, tertile[2], 2,
+      tertile[2] + .00001, Inf, 3),
+    nrow = 3,
+    ncol = 3,
+    byrow = T)
+  ## reclassify distance raster
+  output_rst <- reclassify(x = input_rst,
+                           rcl = rcl_t)
+  output_rst[is.na(output_rst)] <- 0
+  return(output_rst)
+}
+
+################################################################################
+# Create a mask (keep only desired classes as 1, rest NA)
+# input_rst = raster with numeric classes 
+# keep_classes = a vector of desired classes to keep
+################################################################################
+
+create.mask <- function(input_rst, keep_classes){
+  # load packages
+  require(raster)
+  require(dplyr)
+  # sort classes in ascending order
+  class_sort <- sort(keep_classes)
+  # create reclassification matrix
+  ## create proxy matrix with right dimensions
+  rcl <- matrix(
+    nrow = (class_sort %>% length * 2) + 1,
+    ncol = 3
+  )
+  ## populate matrix with values ranging around target classes
+  for (x in class_sort) {
+    
+    rcl[(which(class_sort == x) * 2) - 1, 2] <- x - 2
+    rcl[(which(class_sort == x) * 2) - 1, 3] <- NA
+    
+    rcl[(which(class_sort == x) * 2), 1] <- x - 1
+    rcl[(which(class_sort == x) * 2), 2] <- x + 1
+    rcl[(which(class_sort == x) * 2), 3] <- 1
+    
+    rcl[(which(class_sort == x) * 2) + 1, 1] <- x + 2
+    
+  }
+  ## set starting and ending values of matrix to Infinity
+  rcl[1,1] <- -Inf
+  rcl[rcl[,1] %>% length, 2] <- Inf
+  
+  # reclassify input raster
+  output_rst <- reclassify(x = input_rst,
+                           rcl = rcl)
+  
+  # return mask
+  return(output_rst)
+}
+
+################################################################################
+# Create a land use raster from the urban atlas land use shape file
+################################################################################
+
+rasterize.ua <- function(output_dir, input_shp) {
+  # load required packages
+  require(rgdal)
+  require(dplyr)
+  require(raster)
+  
+  # Create raster file 
+  ## select land-use 2012 code from urban atlas file
+  lu_2012 <- input_shp 
+  lu_2012@data <- 
+    lu_2012 %>% 
+    .@data %>% 
+    select(CODE2012) 
+  ## convert land-use code to numeric values
+  lu_2012@data$CODE2012 <- as.numeric(lu_2012@data$CODE2012)
+  ## create proxy raster for extent and resolution
+  ext_rst <- raster() 
+  extent(ext_rst) <- extent(lu_2012)
+  res(ext_rst) <- 5
+  projection(ext_rst) <- projection(lu_2012)
+  ## Rasterize the urban atlas land use
+  lu_2012.rst <- rasterize(x = lu_2012,
+                           y = ext_rst,
+                           field = lu_2012@data$CODE2012)
+  ## write results to .tif file
+  writeRaster(lu_2012.rst,
+              paste0(output_dir, "LU_2012.tif"),
+              datatype = "INT2U",
+              format = "GTiff",
+              overwrite = T,
+              prj = T,
+              options = c("TFW=YES"))
+  
+  #return result
+  return(lu_2012.rst)
+}
+################################################################################
+
+rasterize.nc <- function(input_dir, output_dir) {
+  # load required packages
+  require(rgdal)
+  require(dplyr)
+  require(raster)
+  
+  # load change and protection files
+  lu_change <- 
+    input_dir %>% 
+    list.files(recursive = T,
+               full.names = T,
+               pattern = ".shp$") %>% 
+    grep("LUC",. ,  value = T) %>% 
+    readOGR()
+  projection(lu_change) <- projection(lu)
+  
+  protection <- 
+    input_dir %>% 
+    list.files(recursive = T,
+               full.names = T,
+               pattern = ".shp$") %>% 
+    grep("protect",. ,  value = T) %>% 
+    readOGR()
+  projection(protection) <- projection(lu)
+  # create dummy raster
+  ext_rst <- raster() 
+  extent(ext_rst) <- extent(lu)
+  res(ext_rst) <- res(lu)
+  projection(ext_rst) <- projection(lu)
+  
+  #rasterize shapefiles (est. 5min per shapefile)
+  change_rst <- rasterize(lu_change_rst, 
+                          ext_rst)
+  prot_rst <- rasterize(protection_rst, 
+                        ext_rst)
+  
+  #remove NA
+  change_rst[is.na(change_rst)] <- 0
+  prot_rst[is.na(prot_rst)] <- 0
+  
+  # create binary map (0/1)
+  no_change <- change_rst + prot_rst
+  no_change[no_change <= 2] <- 0
+  no_change[no_change > 2] <- 1
+  
+  # write no change raster to file
+  writeRaster(no_change,
+              paste0(output_dir, "no_change.tif"),
+              datatype = "INT2S",
+              format = "GTiff",
+              overwrite = T)
+}
